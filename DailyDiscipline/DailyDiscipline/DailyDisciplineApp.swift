046ea361210789f9c044b2776d5d27e5e36ae95b
KraZzez <ludvig.ludde23@gmail.com>
2022-11-18 10:11:42 +0100

    Start

---
+++ DailyDiscipline/DailyDiscipline/DailyDisciplineApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyDisciplineApp.swift
+//  DailyDiscipline
+//
+//  Created by Ludvig Krantzén on 2022-11-18.
+//
+
+import SwiftUI
+
+@main
+struct DailyDisciplineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
