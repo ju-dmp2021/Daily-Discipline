b89905b5aa13fca5d2eb874b1a1cd069095c7c16
Filippos96 <kaha21dg@student.ju.se>
2022-12-07 23:53:06 +0100

    started with tests

---
+++ DailyDiscipline/DailyDiscipline_Tests/DailyDiscipline_Tests.swift
@@ -0,0 +1,47 @@
+//
+//  DailyDiscipline_Tests.swift
+//  DailyDiscipline_Tests
+//
+//  Created by Harry Karl Filip Karlsson on 2022-12-07.
+//
+
+import XCTest
+@testable import DailyDiscipline
+
+
+
+
+//Naming Structure: test_UnitOfWork_StateUnderTest_ExpectedBehaviour
+
+//Testing Structure: Given, When, Then
+
+final class DailyDiscipline_Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func test_NewtaskObjectView_ShowPresetTaskCategory_ShouldBeFalse(){
+        //Given
+        
+        //When
+        
+        //Then
+    }
+
+}
+
+
+//Ideas for tests
+
+//Userlevel = points %10
+//achievements = lvl
+//Levelbar never higher than 10
+//Submit should not add empty title
+//Task must have category name equal to categories (exept for random? (maybe))
+//Tasks should not be named select a category/default value
+//day changes
